7e186791c41393740a7e9fd3666a038bd2b2a247
pilarmartinez <mariapilar.martinez@correo.ucu.edu.uy>
2026-06-16 13:51:34 -0300

    Agrego inicializacion wifi y creo la queue

--- main/proyecto_final.c
+++ main/proyecto_final.c
@@ -1,12 +1,17 @@
 #include "freertos/idf_additions.h"
 #include "freertos/projdefs.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "esp_log.h"
 #include "fsm.h"
-#include <freertos/FreeRTOS.h>
+#include "wifi_manager.h"
+#include "web_server.h"
 
-static QueueHandle_t fsm_event_queue;
+static const char *TAG = "main";
+
+QueueHandle_t fsm_event_queue;
 
 void fsm_task(void *pvParameters) {
-  // EventType event_uuu = (EventType*) pvParameters;
   while (1) {
     EventType incoming_event;
     if (xQueueReceive(fsm_event_queue, &incoming_event, pdMS_TO_TICKS(100)) ==
@@ -18,6 +23,15 @@ void fsm_task(void *pvParameters) {
 }
 
 void app_main(void) {
+  fsm_event_queue = xQueueCreate(20, sizeof(EventType));  // primero la cola
+
   fsm_init();
+
+  ESP_ERROR_CHECK(wifi_manager_init());
+  ESP_ERROR_CHECK(web_server_start());
+
+  ESP_LOGI(TAG, "Conectate a la red %s", wifi_manager_get_ap_ssid());
+  ESP_LOGI(TAG, "Abre en el navegador: http://192.168.4.1");
+
   xTaskCreate(&fsm_task, "FSM_TASK", 4096, NULL, 1, NULL);
 }
